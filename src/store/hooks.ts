cf04e588ea7c19ff5d867d14e068ef96c77c8fc1
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-03 17:59:01 +0200

    import useSelector,type TypedUseSelectorHook from redux to manage selecting data from store

--- src/store/hooks.ts
+++ src/store/hooks.ts
@@ -1,5 +1,11 @@
-import { useDispatch } from "react-redux";
+import {
+  useDispatch,
+  useSelector,
+  type TypedUseSelectorHook,
+} from "react-redux";
 import { AppDispatch } from "./store";
 
 type DispatchFunc = () => AppDispatch;
 export const useCartDispatch: DispatchFunc = useDispatch;
+
+export const useCartSelector: TypedUseSelectorHook = useSelector;
