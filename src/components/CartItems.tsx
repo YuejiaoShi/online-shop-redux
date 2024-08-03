fc3a219eef467f3fb9cfd2db4635a27c09f7812d
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-03 18:21:41 +0200

    conditionally render text or list at CartItems

--- src/components/CartItems.tsx
+++ src/components/CartItems.tsx
@@ -21,27 +21,31 @@ export default function CartItems() {
 
   return (
     <div id="cart">
-      <p>No items in cart!</p>
+      {cartItems.length === 0 && <p>No items in cart!</p>}
 
-      <ul id="cart-items">
-        {cartItems.map((item) => {
-          const formattedPrice = `$${item.price.toFixed(2)}`;
+      {cartItems.length > 0 && (
+        <ul id="cart-items">
+          {cartItems.map((item) => {
+            const formattedPrice = `$${item.price.toFixed(2)}`;
 
-          return (
-            <li key={item.id}>
-              <div>
-                <span>{item.title}</span>
-                <span> ({formattedPrice})</span>
-              </div>
-              <div className="cart-item-actions">
-                <button onClick={() => handleRemoveFromCart(item.id)}>-</button>
-                <span>{item.quantity}</span>
-                <button onClick={() => handleAddToCart(item)}>+</button>
-              </div>
-            </li>
-          );
-        })}
-      </ul>
+            return (
+              <li key={item.id}>
+                <div>
+                  <span>{item.title}</span>
+                  <span> ({formattedPrice})</span>
+                </div>
+                <div className="cart-item-actions">
+                  <button onClick={() => handleRemoveFromCart(item.id)}>
+                    -
+                  </button>
+                  <span>{item.quantity}</span>
+                  <button onClick={() => handleAddToCart(item)}>+</button>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      )}
 
       <p id="cart-total-price">
         Cart Total: <strong>${formattedTotalPrice}</strong>
